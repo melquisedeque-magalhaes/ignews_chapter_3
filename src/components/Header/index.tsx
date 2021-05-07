6c319d8b38d02a54bbf4fadddd20fc7572238b98
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2021-05-07 00:53:53 -0300

    first commit aplication completed

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
+import styles from './styles.module.scss'
+
+export function Header() {
+
+    return(
+        <header className={styles.headerContainer} >
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news"/>
+                <nav>
+
+                    <ActiveLink href='/' activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink href='/posts' activeClassName={styles.active} prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
+                    
+                </nav>
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,77 @@
+import { GetStaticProps } from 'next'
+import { getPrismicClient } from '../../services/prismic'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+import Link from 'next/link'
+
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps){
+    return(
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {
+                        posts.map(post => (
+                            <Link  href={`/posts/preview/${post.slug}`}>
+                                <a key={post.slug}>
+                                    <time>{ post.updatedAt }</time>
+                                    <strong>{ post.title }</strong>
+                                    <p>
+                                        { post.excerpt }
+                                    </p>
+                                </a>
+                            </Link>
+                        ))
+                    }
+                </div>
+            </main>
+        </>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.Predicates.at('document.type', 'publication'),       
+    ],{
+        fetch: ['Publication.title', 'Publication.content'],
+        pageSize: 100
+    })
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
