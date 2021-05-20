c9fd9ad093ea4986ece4d6108359f76107f431f4
Melqui <melqui.sodre15@gmail.com>
2021-05-20 12:45:47 -0300

    resolve error of active subcription propriedade typescript

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,16 @@
 import { signIn, useSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
+import { Session } from 'node:inspector'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string;
+}
+
+interface SessionData extends Session {
+    activeSubscription: null | object;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
